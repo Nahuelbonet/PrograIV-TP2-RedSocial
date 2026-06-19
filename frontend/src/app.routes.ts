7bedb676bfbe2a6999088c56ff414ea24efb10d7
Nahuelbonet <nahuel.a.bonet@gmail.com>
2026-06-19 15:33:22 -0300

    Publicaciones con log

--- frontend/src/app.routes.ts
+++ frontend/src/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginPage } from './pages/login/login-page/login-page';
 import { RegistroPage } from './pages/registro/registro-page/registro-page';
 import { PublicacionesPage } from './pages/publicaciones/publicaciones-page/publicaciones-page';
 import { MiPerfilPage } from './pages/mi-perfil/mi-perfil-page/mi-perfil-page';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/publicaciones', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'registro', component: RegistroPage },
-  { path: 'publicaciones', component: PublicacionesPage },
-  { path: 'mi-perfil', component: MiPerfilPage },
-  { path: '**', redirectTo: '/publicaciones' }
+  { path: 'publicaciones', component: PublicacionesPage, canActivate: [authGuard] },
+  { path: 'mi-perfil', component: MiPerfilPage, canActivate: [authGuard] },
+  { path: '**', redirectTo: '/login' }
 ];
